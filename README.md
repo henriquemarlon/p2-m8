e548f425e1da50ad3081894799972495a39e07bd
Henrique Marlon <henriquemarloncs@gmail.com>
2023-12-15 16:58:00 -0300

    some changes

---
+++ README.md
@@ -0,0 +1,11 @@
+para rodar:
+
+```bash
+$ python3 -m venv venv
+
+```
+
+```bash
+$ pip install playsound soundfile txtai[pipeline]
+```
+
---
+++ main.py
@@ -0,0 +1,22 @@
+import calendar
+import time
+from txtai.pipeline import TextToSpeech
+from playsound import playsound
+import soundfile as sf
+
+current_GMT = time.gmtime()
+
+time_stamp = calendar.timegm(current_GMT)
+
+tts = TextToSpeech("NeuML/ljspeech-jets-onnx")
+
+print("Olá eu sou o Falador")
+
+while True:
+    user_input = input('Escreva as frases e eu falo ')
+
+    speech = tts(user_input)
+
+    sf.write(f"./assets/out{time_stamp}.wav", speech, 22050)
+
+    playsound(f'./assets/out{time_stamp}.wav')
